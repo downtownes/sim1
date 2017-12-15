5545d6116af03796e4a3d874f5c82e69d976ee17
unknown <townes.falcon@gmail.com>
2017-12-15 14:38:15 -0700

    server connected

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    massive = require('massive')
+
+
+    
+const app = express();
+
+
+massive(process.env.CONNECTIONSTRING).then(db  => {
+        app.set('db', db)
+        // app.get('db').init.seed_file().then(response => {
+        //     console.log(response)
+        // })
+    })
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`)
+})
